e0e21da21569facfea054d4ea583cbb656b869a3
Andrew Fan <andrewsuicune@gmail.com>
2018-09-02 15:41:57 -0400

    Completed 3 line basic example network

--- test-network/line_test.js
+++ test-network/line_test.js
@@ -1,5 +1,5 @@
 var obj_lineA = {
-    "linename": "Line A",
+    "linename": "Line A - Avenue C Line",
     "iconID": "ICON_LINEA_BIG",
     "color": "#FF0000",
     "strokewidth": "8px",
@@ -12,7 +12,7 @@ var obj_lineA = {
             ]
         },
         {
-            "name": "2nd Street",
+            "name": "2nd Street - Avenue C",
             "icons": [
                 "ICON_LINEB",
                 "ICON_LINEC"
@@ -29,9 +29,141 @@ var obj_lineA = {
             ]
         },
         {
-            "name": "5th Street",
+            "name": "5th Street - Avenue C",
             "icons": [
                 "ICON_LINEB"
+            ]
+        },
+        {
+            "name": "6th Street",
+            "icons": [
+            ]
+        },
+        {
+            "name": "7th Street",
+            "icons": [
+            ]
+        },
+        {
+            "name": "8th Street",
+            "icons": [
+            ]
+        },
+        {
+            "name": "9th Street",
+            "icons": [
+            ]
+        },
+        {
+            "name": "Zoo",
+            "icons": [
+            ]
+        }
+    ]
+};
+
+var obj_lineB = {
+    "linename": "Line B - 2nd Street Line",
+    "iconID": "ICON_LINEB_BIG",
+    "color": "#0000FF",
+    "strokewidth": "8px",
+    "stationstrokewidth": 2,
+    "stationradius": 8,
+    "stations": [
+        {
+            "name": "Avenue A",
+            "icons": [
+            ]
+        },
+        {
+            "name": "Avenue B",
+            "icons": [
+            ]
+        },
+        {
+            "name": "2nd Street - Avenue C",
+            "icons": [
+                "ICON_LINEA",
+                "ICON_LINEC"
+            ]
+        },
+        {
+            "name": "3rd Street - Avenue D",
+            "icons": [
+            ]
+        },
+        {
+            "name": "City Hall",
+            "icons": [
+            ]
+        },
+        {
+            "name": "5th Street - Avenue C",
+            "icons": [
+                "ICON_LINEA"
+            ]
+        }
+    ]
+};
+
+var obj_lineC = {
+    "linename": "Line C - South River Line",
+    "iconID": "ICON_LINEC_BIG",
+    "color": "#00FF00",
+    "strokewidth": "8px",
+    "stationstrokewidth": 2,
+    "stationradius": 8,
+    "stations": [
+        {
+            "name": "Avenue A Bridge",
+            "icons": [
+            ]
+        },
+        {
+            "name": "Avenue B Bridge",
+            "icons": [
+            ]
+        },
+        {
+            "name": "2nd Street - Avenue C",
+            "icons": [
+                "ICON_LINEA",
+                "ICON_LINEB"
+            ]
+        },
+        {
+            "name": "Avenue D Bridge",
+            "icons": [
+            ]
+        },
+        {
+            "name": "Avenue E",
+            "icons": [
+            ]
+        },
+        {
+            "name": "Avenue F",
+            "icons": [
+            ]
+        },
+        {
+            "name": "Airport North",
+            "icons": [
+            ]
+        },
+        {
+            "name": "Airport Terminal 1",
+            "icons": [
+            ]
+        },
+        {
+            "name": "Airport Terminal 2",
+            "icons": [
+            ]
+        },
+        {
+            "name": "Navy Yard",
+            "icons": [
             ]
         }
     ]
