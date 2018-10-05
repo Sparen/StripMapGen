81915d4be39f07e12fb15dbbace03c1488468d82
Andrew Fan <andrewsuicune@gmail.com>
2018-10-05 19:08:17 -0400

    Main file changed from var to let/const

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
 
 // This function takes a line object and icon object (required to include those scripts) and outputs to the specified target div
 function loadMap(lineobj, iconobj, targetdiv) {
-    var smsvg = '<svg preserveAspectRatio="xMinYMin meet" viewBox="0 0 2000 480" version="1.1" xmlns="http://www.w3.org/2000/svg">';
+    let smsvg = '<svg preserveAspectRatio="xMinYMin meet" viewBox="0 0 2000 480" version="1.1" xmlns="http://www.w3.org/2000/svg">';
     smsvg += '<rect width="2000" height="480" fill="white" />';
 
     // Define def patterns for icons. Loading all of them.
     smsvg += '<defs>';
-    for (var i = 0; i < iconobj.icons.length; i += 1) {
-        var lineIconSmall = iconobj.icons[i];
+    for (let i = 0; i < iconobj.icons.length; i += 1) {
+        let lineIconSmall = iconobj.icons[i];
         smsvg += '<pattern id="PATTERN_' + lineIconSmall.iconID + '" x="0" y="0" width="100%" height="100%" patternUnits="objectBoundingBox">';
         smsvg += lineIconSmall.iconSVG;
         smsvg += '</pattern>';
@@ -20,16 +20,16 @@ function loadMap(lineobj, iconobj, targetdiv) {
     smsvg += '<text x="80" y="48" font-family="Arial" font-size="32px" fill="black" font-weight="bold" text-anchor="start" dominant-baseline="central">' + lineobj.linename + '</text>';
 
     // Number of stations. Used for spacing and placement
-    var numstations = lineobj.stations.length;
+    let numstations = lineobj.stations.length;
 
     // Next, draw the line strokes
-    for (var i = 0; i < lineobj.strokes.length; i += 1) {
-        var lineStroke = lineobj.strokes[i];
+    for (let i = 0; i < lineobj.strokes.length; i += 1) {
+        let lineStroke = lineobj.strokes[i];
         // Defaults
-        var LINE_LEFT = 128;
-        var LINE_RIGHT = 1600;
-        var strokeStart = LINE_LEFT;
-        var strokeEnd = LINE_RIGHT;
+        const LINE_LEFT = 128;
+        const LINE_RIGHT = 1600;
+        let strokeStart = LINE_LEFT;
+        let strokeEnd = LINE_RIGHT;
         // Note: It is possible to extend the line beyond the set bounds of 128 and 1600
         if ("startpoint" in lineStroke) {
             strokeStart = LINE_LEFT + lineStroke.startpoint * 1472/(numstations - 1);
@@ -37,12 +37,12 @@ function loadMap(lineobj, iconobj, targetdiv) {
         if ("endpoint" in lineStroke) {
             strokeEnd = LINE_LEFT + lineStroke.endpoint * 1472/(numstations - 1);
         }
-        var ycoord = 240;
+        let ycoord = 240;
         if ("dy" in lineStroke) {
             ycoord += lineStroke.dy;
         }
         // Additional parameters to strokes
-        var additionalParameters = " ";
+        let additionalParameters = " ";
         if ("dasharray" in lineStroke) {
             additionalParameters += 'stroke-dasharray="' + lineStroke.dasharray + '" ';
         }
@@ -58,11 +58,11 @@ function loadMap(lineobj, iconobj, targetdiv) {
 
     // Next, add the stations, their icons, and their names, rotated 45 degrees
     // Note that station information must be retrieved
-    for(var i = 0; i < numstations; i += 1) {
-        var currstn = lineobj.stations[i];
+    for(let i = 0; i < numstations; i += 1) {
+        let currstn = lineobj.stations[i];
         // First, before drawing, determine station features
-        var stntypeobj;
-        for (var j = 0; j < lineobj.stationtypes.length; j += 1) {
+        let stntypeobj;
+        for (let j = 0; j < lineobj.stationtypes.length; j += 1) {
             if (lineobj.stationtypes[j].stypeID == currstn.stationtype) {
                 stntypeobj = lineobj.stationtypes[j];
             }
@@ -72,23 +72,23 @@ function loadMap(lineobj, iconobj, targetdiv) {
             return;
         }
         // Handle offsets
-        var ycoord = 240;
-        var iconycoord = 256;
+        let ycoord = 240;
+        let iconycoord = 256;
         if ("dy" in currstn) {
             ycoord += currstn.dy;
             if ("iconshift" in currstn && currstn.iconshift) {
                 iconycoord += currstn.dy;
             }
         }
-        var xshift = 0;
+        let xshift = 0;
         if ("xshift" in currstn) { // Shifts are relative to station positions rather than absolute
             xshift += 1472/(numstations - 1) * currstn.xshift;
         }
         // Draw
         smsvg += '<circle cx="' + (128 + xshift + 1472/(numstations - 1) * i) + '" cy="' + ycoord + '" r="' + stntypeobj.stationradius + '" stroke="' + stntypeobj.scolor + '" stroke-width="' + stntypeobj.stationstrokewidth + '" fill="white"></circle>';
         smsvg += '<text x="' + (128 + xshift + 1472/(numstations - 1) * i) + '" y="' + (ycoord - 16) + '" font-family="Arial" font-size="16px" fill="black" font-weight="bold" text-anchor="start" dominant-baseline="alphabetic" transform="rotate(-45 ' + (128 + xshift + 1472/(numstations - 1) * i) + ' ' + (ycoord - 16) + ')">' + currstn.name + '</text>';
-        var stationIcons = currstn.icons;
-        for (var j = 0; j < stationIcons.length; j += 1) {
+        let stationIcons = currstn.icons;
+        for (let j = 0; j < stationIcons.length; j += 1) {
             smsvg += '<rect x="' + (128 + xshift + 1472/(numstations - 1) * i - 16) + '" y="' + (iconycoord + 36*j) + '" height="32" width="32" fill="url(#PATTERN_' + stationIcons[j] + ')" />';
         }
     }
