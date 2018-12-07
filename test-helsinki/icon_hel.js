39bc1ac15f689107455fbe520b0b9145c46d52b6
Andrew Fan <andrewsuicune@gmail.com>
2018-12-07 14:22:30 -0500

    Added provisions for new icon scaling system

--- test-helsinki/icon_hel.js
+++ test-helsinki/icon_hel.js
@@ -2,18 +2,30 @@ var obj_iconHel = {
     "icons": [
         {
             "iconID": "ICON_LINEM_BIG",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
             "iconSVG": "<rect x='0' y='0' width='48' height='48' rx='6' ry='6' stroke='none' fill='#FF6600'></rect><text x='24' y='24' font-family='Arial' font-size='48px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>M</text>"
         },
         {
             "iconID": "ICON_LINEM",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
             "iconSVG": "<rect x='0' y='0' width='32' height='32' rx='6' ry='6' stroke='none' fill='#FF6600'></rect><text x='16' y='16' font-family='Arial' font-size='32px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>M</text>"
         },
         {
             "iconID": "ICON_LINEM1",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
             "iconSVG": "<rect x='0' y='0' width='32' height='32' rx='6' ry='6' stroke='none' fill='#FF6600'></rect><text x='16' y='16' font-family='Arial' font-size='16px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>M1</text>"
         },
         {
             "iconID": "ICON_LINEM2",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
             "iconSVG": "<rect x='0' y='0' width='32' height='32' rx='6' ry='6' stroke='none' fill='#FF6600'></rect><text x='16' y='16' font-family='Arial' font-size='16px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>M2</text>"
         }
     ]
--- test-network/icon_test.js
+++ test-network/icon_test.js
@@ -2,34 +2,58 @@ var obj_iconTest = {
     "icons": [
         {
             "iconID": "ICON_LINEA_BIG",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
             "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='#FF0000' fill='#FFFFFF'></circle><text x='24' y='24' font-family='Arial' font-size='18px' fill='black' font-weight='bold' text-anchor='middle' dominant-baseline='central'>A</text>"
         },
         {
             "iconID": "ICON_LINEB_BIG",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
             "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='#0000FF' fill='#FFFFFF'></circle><text x='24' y='24' font-family='Arial' font-size='18px' fill='black' font-weight='bold' text-anchor='middle' dominant-baseline='central'>B</text>"
         },
         {
             "iconID": "ICON_LINEC_BIG",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
             "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='#44CC44' fill='#FFFFFF'></circle><text x='24' y='24' font-family='Arial' font-size='18px' fill='black' font-weight='bold' text-anchor='middle' dominant-baseline='central'>C</text>"
         },
         {
             "iconID": "ICON_LINED_BIG",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
             "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='#CC00CC' fill='#FFFFFF'></circle><text x='24' y='24' font-family='Arial' font-size='18px' fill='black' font-weight='bold' text-anchor='middle' dominant-baseline='central'>D</text>"
         },
         {
             "iconID": "ICON_LINEA",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
             "iconSVG": "<circle cx='16' cy='16' r='12' stroke-width='4px' stroke='#FF0000' fill='#FFFFFF'></circle><text x='16' y='16' font-family='Arial' font-size='12px' fill='black' font-weight='bold' text-anchor='middle' dominant-baseline='central'>A</text>"
         },
         {
             "iconID": "ICON_LINEB",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
             "iconSVG": "<circle cx='16' cy='16' r='12' stroke-width='4px' stroke='#0000FF' fill='#FFFFFF'></circle><text x='16' y='16' font-family='Arial' font-size='12px' fill='black' font-weight='bold' text-anchor='middle' dominant-baseline='central'>B</text>"
         },
         {
             "iconID": "ICON_LINEC",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
             "iconSVG": "<circle cx='16' cy='16' r='12' stroke-width='4px' stroke='#44CC44' fill='#FFFFFF'></circle><text x='16' y='16' font-family='Arial' font-size='12px' fill='black' font-weight='bold' text-anchor='middle' dominant-baseline='central'>C</text>"
         },
         {
             "iconID": "ICON_LINED",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
             "iconSVG": "<circle cx='16' cy='16' r='12' stroke-width='4px' stroke='#CC00CC' fill='#FFFFFF'></circle><text x='16' y='16' font-family='Arial' font-size='12px' fill='black' font-weight='bold' text-anchor='middle' dominant-baseline='central'>D</text>"
         }
     ]
