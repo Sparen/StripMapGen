fc7e0589ef207b18881dd4bd4abd2a5753ba04d5
Andrew Fan <andrewsuicune@gmail.com>
2018-12-07 14:21:14 -0500

    Made icon folder, assembled NYC icons as well as new scaling tech for icons s.t. only one icon scale is needed

---
+++ icon-common/icon_nyc.js
@@ -0,0 +1,247 @@
+var obj_iconNYC_IRT = {
+    "icons": [
+        {
+            "iconID": "ICON_1_RED",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#EE352E'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>1</text>"
+        },
+        {
+            "iconID": "ICON_2_RED",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#EE352E'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>2</text>"
+        },
+        {
+            "iconID": "ICON_3_RED",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#EE352E'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>3</text>"
+        },
+        {
+            "iconID": "ICON_4_GREEN",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#00933C'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>4</text>"
+        },
+        {
+            "iconID": "ICON_5_GREEN",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#00933C'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>5</text>"
+        },
+        {
+            "iconID": "ICON_6_GREEN",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#00933C'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>6</text>"
+        },
+        {
+            "iconID": "ICON_7_PURPLE",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#B933AD'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>7</text>"
+        },
+        {
+            "iconID": "ICON_8_AQUA",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#00ADD0'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>8</text>"
+        },
+        {
+            "iconID": "ICON_9_RED",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#EE352E'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>9</text>"
+        },
+        {
+            "iconID": "ICON_10_GREEN",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#00933C'></circle><text x='24' y='24' font-family='Arial' font-size='20px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>10</text>"
+        },
+        {
+            "iconID": "ICON_11_PURPLE",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#B933AD'></circle><text x='24' y='24' font-family='Arial' font-size='20px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>11</text>"
+        },
+        {
+            "iconID": "ICON_12_GREEN",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#00933C'></circle><text x='24' y='24' font-family='Arial' font-size='20px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>12</text>"
+        }
+    ]
+};
+
+var obj_iconNYC_BMTIND = {
+    "icons": [
+        {
+            "iconID": "ICON_A_BLUE",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#2850AD'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>A</text>"
+        },
+        {
+            "iconID": "ICON_B_ORANGE",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#FF6319'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>B</text>"
+        },
+        {
+            "iconID": "ICON_C_BLUE",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#2850AD'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>C</text>"
+        },
+        {
+            "iconID": "ICON_D_ORANGE",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#FF6319'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>D</text>"
+        },
+        {
+            "iconID": "ICON_E_BLUE",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#2850AD'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>E</text>"
+        },
+        {
+            "iconID": "ICON_F_ORANGE",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#FF6319'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>F</text>"
+        },
+        {
+            "iconID": "ICON_G_LIME",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#6CBE45'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>G</text>"
+        },
+        {
+            "iconID": "ICON_H_BLUE",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#2850AD'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>H</text>"
+        },
+        {
+            "iconID": "ICON_J_BROWN",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#996633'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>J</text>"
+        },
+        {
+            "iconID": "ICON_K_BLUE",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#2850AD'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>K</text>"
+        },
+        {
+            "iconID": "ICON_L_GRAY",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#A7A9AC'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>L</text>"
+        },
+        {
+            "iconID": "ICON_M_ORANGE",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#FF6319'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>M</text>"
+        },
+        {
+            "iconID": "ICON_M_BROWN",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#996633'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>M</text>"
+        },
+        {
+            "iconID": "ICON_N_YELLOW",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#FCCC0A'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='#333' font-weight='bold' text-anchor='middle' dominant-baseline='central'>N</text>"
+        },
+        {
+            "iconID": "ICON_Q_YELLOW",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#FCCC0A'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='#333' font-weight='bold' text-anchor='middle' dominant-baseline='central'>Q</text>"
+        },
+        {
+            "iconID": "ICON_R_YELLOW",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#FCCC0A'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='#333' font-weight='bold' text-anchor='middle' dominant-baseline='central'>R</text>"
+        },
+        {
+            "iconID": "ICON_S_GRAY",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#808183'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>S</text>"
+        },
+        {
+            "iconID": "ICON_T_TURQUOISE",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#1E9DBF'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>T</text>"
+        },
+        {
+            "iconID": "ICON_U_TURQUOISE",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#1E9DBF'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>U</text>"
+        },
+        {
+            "iconID": "ICON_V_ORANGE",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#FF6319'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>V</text>"
+        },
+        {
+            "iconID": "ICON_W_YELLOW",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#FCCC0A'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='#333' font-weight='bold' text-anchor='middle' dominant-baseline='central'>W</text>"
+        },
+        {
+            "iconID": "ICON_Z_BROWN",
+            "height": 48,
+            "width": 48,
+            "scale": [1.0, 0.67],
+            "iconSVG": "<circle cx='24' cy='24' r='18' stroke-width='4px' stroke='none' fill='#996633'></circle><text x='24' y='24' font-family='Arial' font-size='22px' fill='white' font-weight='bold' text-anchor='middle' dominant-baseline='central'>Z</text>"
+        }
+    ]
+};
---
+++ icon-common/render.js
@@ -0,0 +1,25 @@
+"use strict"
+
+function render() {
+    // NOTE: Hardcoded!
+    render_block(obj_iconNYC_IRT, "div_nyc1");
+    render_block(obj_iconNYC_BMTIND, "div_nyc2");
+}
+
+function render_block(icon, targetdiv) {
+    let output = "";
+    // Full size scaling
+    for (var i = 0; i < icon.icons.length; i += 1) {
+        let iobj = icon.icons[i];
+        let sc = iobj.scale[0];
+        output += "<svg height=" + iobj.height * sc + " width=" + iobj.width * sc + "><g transform='scale(" + sc + " " + sc + ")'>" + iobj.iconSVG + "</g></svg>";
+    }
+    output += "<br>";
+    // Small scaling
+    for (var i = 0; i < icon.icons.length; i += 1) {
+        let iobj = icon.icons[i];
+        let sc = iobj.scale[1];
+        output += "<svg height=" + iobj.height * sc + " width=" + iobj.width * sc + "><g transform='scale(" + sc + " " + sc + ")'>" + iobj.iconSVG + "</g></svg>";
+    }
+    document.getElementById(targetdiv).innerHTML = output;
+}
