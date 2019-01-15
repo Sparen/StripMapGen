a046e21ef5221bcdc5e2a9999165552f194a63ce
Andrew Fan <andrewsuicune@gmail.com>
2019-01-15 11:55:54 -0500

    Unit 1 written. Fixed #11

--- user-guide/line_u1.js
+++ user-guide/line_u1.js
@@ -226,3 +226,695 @@ var obj_line1_v1 = {
         }
     ]
 };
+
+var obj_line1_v2 = {
+    "linename": "London Underground Jubilee Line",
+    "iconID": ["ICON_LONDON_JUBILEE"],
+    "strokes": [
+        {
+            "color": "#a1a5a7",
+            "strokewidth": "8px"
+        }
+    ],
+    "stationtypes": [
+        {
+            "stypeID": "STATION_TYPEA",
+            "stnnodes": [
+                {
+                    "stationradius": 8,
+                    "stationstrokewidth": 3,
+                    "fcolor": "white",
+                    "scolor": "#a1a5a7",
+                }
+            ]
+        }
+    ],
+    "stations": [
+        {
+            "name": ["Stanmore"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Stratford"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        }
+    ]
+};
+
+var obj_line1_v3 = {
+    "linename": "London Underground Jubilee Line",
+    "iconID": ["ICON_LONDON_JUBILEE"],
+    "strokes": [
+        {
+            "color": "#a1a5a7",
+            "strokewidth": "8px"
+        }
+    ],
+    "stationtypes": [
+        {
+            "stypeID": "STATION_TYPEA",
+            "stnnodes": [
+                {
+                    "componenttype": "RECT",
+                    "stationwidth": 3,
+                    "stationheight": 10,
+                    "scolor": "#a1a5a7",
+                    "dy": -3
+                }
+            ]
+        },
+        {
+            "stypeID": "STATION_TYPETRANSFER",
+            "stnnodes": [
+                {
+                    "stationradius": 8,
+                    "stationstrokewidth": 3,
+                    "fcolor": "white",
+                    "scolor": "black",
+                }
+            ]
+        }
+    ],
+    "stations": [
+        {
+            "name": ["Stanmore"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Canons Park"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Queensbury"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Kingsbury"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Wembley Park"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Neasden"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Dollis Hill"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Willesden Green"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Kilburn"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["West Hampstead"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["FInchley Road"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Swiss Cottage"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["St John's Wood"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Baker Street"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Bond Street"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Green Park"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Westminster"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Waterloo"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Southwark"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["London Bridge"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Bermondsey"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Canada Water"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Canary Wharf"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["North Greenwich"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Canning Town"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["West Ham"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Stratford"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+            ]
+        }
+    ]
+};
+
+var obj_line1_v4 = {
+    "linename": "London Underground Jubilee Line",
+    "iconID": ["ICON_LONDON_JUBILEE"],
+    "strokes": [
+        {
+            "color": "#a1a5a7",
+            "strokewidth": "8px"
+        }
+    ],
+    "stationtypes": [
+        {
+            "stypeID": "STATION_TYPEA",
+            "stnnodes": [
+                {
+                    "componenttype": "RECT",
+                    "stationwidth": 3,
+                    "stationheight": 10,
+                    "scolor": "#a1a5a7",
+                    "dy": -3
+                }
+            ]
+        },
+        {
+            "stypeID": "STATION_TYPETRANSFER",
+            "stnnodes": [
+                {
+                    "stationradius": 8,
+                    "stationstrokewidth": 3,
+                    "fcolor": "white",
+                    "scolor": "black",
+                }
+            ]
+        }
+    ],
+    "stations": [
+        {
+            "name": ["Stanmore"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Canons Park"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Queensbury"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Kingsbury"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Wembley Park"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_METROPOLITAN"]
+            ]
+        },
+        {
+            "name": ["Neasden"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Dollis Hill"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Willesden Green"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Kilburn"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["West Hampstead"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_OVERGROUND"]
+            ]
+        },
+        {
+            "name": ["FInchley Road"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_METROPOLITAN"]
+            ]
+        },
+        {
+            "name": ["Swiss Cottage"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["St John's Wood"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Baker Street"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_BAKERLOO"],
+                ["ICON_LONDON_CIRCLE"],
+                ["ICON_LONDON_H&C"],
+                ["ICON_LONDON_METROPOLITAN"]
+            ]
+        },
+        {
+            "name": ["Bond Street"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_CENTRAL"]
+            ]
+        },
+        {
+            "name": ["Green Park"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_PICCADILLY"],
+                ["ICON_LONDON_VICTORIA"]
+            ]
+        },
+        {
+            "name": ["Westminster"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_CIRCLE"],
+                ["ICON_LONDON_DISTRICT"]
+            ]
+        },
+        {
+            "name": ["Waterloo"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_BAKERLOO"],
+                ["ICON_LONDON_NORTHERN"],
+                ["ICON_LONDON_W&C"]
+            ]
+        },
+        {
+            "name": ["Southwark"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["London Bridge"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_NORTHERN"]
+            ]
+        },
+        {
+            "name": ["Bermondsey"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Canada Water"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_OVERGROUND"]
+            ]
+        },
+        {
+            "name": ["Canary Wharf"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_DLR"]
+            ]
+        },
+        {
+            "name": ["North Greenwich"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Canning Town"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_DLR"]
+            ]
+        },
+        {
+            "name": ["West Ham"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_DISTRICT"],
+                ["ICON_LONDON_H&C"],
+                ["ICON_LONDON_DLR"]
+            ]
+        },
+        {
+            "name": ["Stratford"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_CENTRAL"],
+                ["ICON_LONDON_DLR"],
+                ["ICON_LONDON_OVERGROUND"],
+                ["ICON_LONDON_TFLRAIL"]
+            ]
+        }
+    ]
+};
+
+var obj_line1_v5 = {
+    "linename": "London Underground Jubilee Line",
+    "iconID": ["ICON_LONDON_JUBILEE"],
+    "linestart": 64,
+    "lineend": 1920,
+    "strokes": [
+        {
+            "color": "#a1a5a7",
+            "strokewidth": "8px"
+        }
+    ],
+    "stationtypes": [
+        {
+            "stypeID": "STATION_TYPEA",
+            "stnnodes": [
+                {
+                    "componenttype": "RECT",
+                    "stationwidth": 3,
+                    "stationheight": 10,
+                    "scolor": "#a1a5a7",
+                    "dy": -3
+                }
+            ]
+        },
+        {
+            "stypeID": "STATION_TYPETRANSFER",
+            "stnnodes": [
+                {
+                    "stationradius": 8,
+                    "stationstrokewidth": 3,
+                    "fcolor": "white",
+                    "scolor": "black",
+                }
+            ]
+        }
+    ],
+    "stations": [
+        {
+            "name": ["Stanmore"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Canons Park"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Queensbury"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Kingsbury"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Wembley Park"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_METROPOLITAN"]
+            ]
+        },
+        {
+            "name": ["Neasden"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Dollis Hill"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Willesden Green"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Kilburn"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["West Hampstead"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_OVERGROUND"]
+            ]
+        },
+        {
+            "name": ["FInchley Road"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_METROPOLITAN"]
+            ]
+        },
+        {
+            "name": ["Swiss Cottage"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["St John's Wood"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Baker Street"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_BAKERLOO"],
+                ["ICON_LONDON_CIRCLE"],
+                ["ICON_LONDON_H&C"],
+                ["ICON_LONDON_METROPOLITAN"]
+            ]
+        },
+        {
+            "name": ["Bond Street"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_CENTRAL"]
+            ]
+        },
+        {
+            "name": ["Green Park"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_PICCADILLY"],
+                ["ICON_LONDON_VICTORIA"]
+            ]
+        },
+        {
+            "name": ["Westminster"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_CIRCLE"],
+                ["ICON_LONDON_DISTRICT"]
+            ]
+        },
+        {
+            "name": ["Waterloo"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_BAKERLOO"],
+                ["ICON_LONDON_NORTHERN"],
+                ["ICON_LONDON_W&C"]
+            ]
+        },
+        {
+            "name": ["Southwark"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["London Bridge"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_NORTHERN"]
+            ]
+        },
+        {
+            "name": ["Bermondsey"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Canada Water"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_OVERGROUND"]
+            ]
+        },
+        {
+            "name": ["Canary Wharf"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_DLR"]
+            ]
+        },
+        {
+            "name": ["North Greenwich"],
+            "stationtype": "STATION_TYPEA",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Canning Town"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_DLR"]
+            ]
+        },
+        {
+            "name": ["West Ham"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_DISTRICT"],
+                ["ICON_LONDON_H&C"],
+                ["ICON_LONDON_DLR"]
+            ]
+        },
+        {
+            "name": ["Stratford"],
+            "stationtype": "STATION_TYPETRANSFER",
+            "icons": [
+                ["ICON_LONDON_CENTRAL"],
+                ["ICON_LONDON_DLR"],
+                ["ICON_LONDON_OVERGROUND"],
+                ["ICON_LONDON_TFLRAIL"]
+            ]
+        }
+    ]
+};
