1e696bd11928845a52403b539206a578846a308d
Andrew Fan <andrewsuicune@gmail.com>
2019-01-22 14:09:32 -0500

    User Guide Unit 2 complete. Fixed #12

--- user-guide/line_u2.js
+++ user-guide/line_u2.js
@@ -97,8 +97,7 @@ var obj_line2_v1 = {
                     "fcolor": "white",
                     "scolor": "none"
                 }
-            ],
-            "stntextoffset": 16
+            ]
         },
         {
             "stypeID": "STATION_TYPEM2",
@@ -285,5 +284,466 @@ var obj_line2_v1 = {
         }
     ]
 };
+
+var obj_line2_v2 = {
+    "linename": "Copenhagen Metro",
+    "iconID": ["ICON_M1", "ICON_M2"],
+    "lineend": 1800,
+    "maincustomsvgbg": "<text x='16' y='460' font-family='DIN Alternate' font-size='20px' fill='#AAAAAA' text-anchor='start' dominant-baseline='central'>01/2019</text>",
+    "strokes": [
+        {
+            "color": "#397c5d",
+            "strokewidth": "12px",
+            "linecap": "square",
+            "dy": 16
+        },
+        {
+            "color": "#ebd043",
+            "strokewidth": "12px",
+            "linecap": "square",
+            "endpoint": 15
+        }
+    ],
+    "stationtypes": [
+        {
+            "stypeID": "STATION_TYPEM1",
+            "stnnodes": [
+                {
+                    "stationradius": 4,
+                    "stationstrokewidth": 0,
+                    "fcolor": "white",
+                    "scolor": "none",
+                    "dy": 16
+                }
+            ],
+            "stniconoffset": 32
+        },
+        {
+            "stypeID": "STATION_TYPEM2",
+            "stnnodes": [
+                {
+                    "stationradius": 4,
+                    "stationstrokewidth": 0,
+                    "fcolor": "black",
+                    "scolor": "none"
+                }
+            ],
+            "stniconoffset": 32
+        },
+        {
+            "stypeID": "STATION_TYPE_COMBINED",
+            "stnnodes": [
+                {
+                    "stationradius": 4,
+                    "stationstrokewidth": 0,
+                    "fcolor": "black",
+                    "scolor": "none"
+                },
+                {
+                    "stationradius": 4,
+                    "stationstrokewidth": 0,
+                    "fcolor": "white",
+                    "scolor": "none",
+                    "dy": 16
+                }
+            ],
+            "stniconoffset": 32
+        }
+    ],
+    "stations": [
+        {
+            "name": ["Vanløse"],
+            "stationtype": "STATION_TYPE_COMBINED",
+            "icons": [
+                ["S-tog"]
+            ]
+        },
+        {
+            "name": ["Flintholm"],
+            "stationtype": "STATION_TYPE_COMBINED",
+            "icons": [
+                ["S-tog"]
+            ]
+        },
+        {
+            "name": ["Lindevang"],
+            "stationtype": "STATION_TYPE_COMBINED",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Fasanvej"],
+            "stationtype": "STATION_TYPE_COMBINED",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Frederiksberg"],
+            "stationtype": "STATION_TYPE_COMBINED",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Forum"],
+            "stationtype": "STATION_TYPE_COMBINED",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Nørreport"],
+            "stationtype": "STATION_TYPE_COMBINED",
+            "icons": [
+                ["S-tog"],
+                ["DSB"]
+            ]
+        },
+        {
+            "name": ["Kongens Nytorv"],
+            "stationtype": "STATION_TYPE_COMBINED",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Christianshavn"],
+            "stationtype": "STATION_TYPE_COMBINED",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Amagerbro"],
+            "stationtype": "STATION_TYPEM2",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Lergravsparken"],
+            "stationtype": "STATION_TYPEM2",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Øresund"],
+            "stationtype": "STATION_TYPEM2",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Amager Strand"],
+            "stationtype": "STATION_TYPEM2",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Femøren"],
+            "stationtype": "STATION_TYPEM2",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Kastrup"],
+            "stationtype": "STATION_TYPEM2",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Lufthavnen"],
+            "stationtype": "STATION_TYPEM2",
+            "icons": [
+                ["DSB"]
+            ]
+        },
+        {
+            "name": ["Islands Brygge"],
+            "stationtype": "STATION_TYPEM1",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["DR Byen"],
+            "stationtype": "STATION_TYPEM1",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Sundby"],
+            "stationtype": "STATION_TYPEM1",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Bella Center"],
+            "stationtype": "STATION_TYPEM1",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Ørestad"],
+            "stationtype": "STATION_TYPEM1",
+            "icons": [
+                ["DSB"]
+            ]
+        },
+        {
+            "name": ["Vestamager"],
+            "stationtype": "STATION_TYPEM1",
+            "icons": [
+            ]
+        }
+    ]
+};
+
+var obj_line2_v3 = {
+    "linename": "Copenhagen Metro",
+    "iconID": ["ICON_M1", "ICON_M2"],
+    "lineend": 1800,
+    "maincustomsvgbg": "<text x='16' y='460' font-family='DIN Alternate' font-size='20px' fill='#AAAAAA' text-anchor='start' dominant-baseline='central'>01/2019</text>",
+    "strokes": [
+        {
+            "color": "#397c5d",
+            "strokewidth": "12px",
+            "linecap": "square",
+            "dy": 16,
+            "endpoint": 8.25
+        },
+        {
+            "color": "#ebd043",
+            "strokewidth": "12px",
+            "linecap": "square",
+            "endpoint": 8.25
+        },
+        {
+            "color": "#397c5d",
+            "strokewidth": "12px",
+            "linecap": "round",
+            "dy": 16,
+            "startpoint": 8.25,
+            "endpoint": 8.75,
+            "endlinkheight": 16
+        },
+        {
+            "color": "#ebd043",
+            "strokewidth": "12px",
+            "linecap": "round",
+            "startpoint": 8.25,
+            "endpoint": 8.75,
+            "endlinkheight": -16
+        },
+        {
+            "color": "#397c5d",
+            "strokewidth": "12px",
+            "linecap": "square",
+            "dy": 16,
+            "startpoint": 8.75,
+            "startlinkheight": 16,
+            "endlinkheight": 16
+        },
+        {
+            "color": "#ebd043",
+            "strokewidth": "12px",
+            "linecap": "square",
+            "startpoint": 8.75,
+            "endpoint": 15,
+            "startlinkheight": -16,
+            "endlinkheight": -16
+        }
+    ],
+    "stationtypes": [
+        {
+            "stypeID": "STATION_TYPEM1",
+            "stnnodes": [
+                {
+                    "stationradius": 4,
+                    "stationstrokewidth": 0,
+                    "fcolor": "white",
+                    "scolor": "none"
+                }
+            ]
+        },
+        {
+            "stypeID": "STATION_TYPEM2",
+            "stnnodes": [
+                {
+                    "stationradius": 4,
+                    "stationstrokewidth": 0,
+                    "fcolor": "black",
+                    "scolor": "none"
+                }
+            ]
+        },
+        {
+            "stypeID": "STATION_TYPE_COMBINED",
+            "stnnodes": [
+                {
+                    "stationradius": 4,
+                    "stationstrokewidth": 0,
+                    "fcolor": "black",
+                    "scolor": "none"
+                },
+                {
+                    "stationradius": 4,
+                    "stationstrokewidth": 0,
+                    "fcolor": "white",
+                    "scolor": "none",
+                    "dy": 16
+                }
+            ],
+            "stniconoffset": 32
+        }
+    ],
+    "stations": [
+        {
+            "name": ["Vanløse"],
+            "stationtype": "STATION_TYPE_COMBINED",
+            "icons": [
+                ["S-tog"]
+            ]
+        },
+        {
+            "name": ["Flintholm"],
+            "stationtype": "STATION_TYPE_COMBINED",
+            "icons": [
+                ["S-tog"]
+            ]
+        },
+        {
+            "name": ["Lindevang"],
+            "stationtype": "STATION_TYPE_COMBINED",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Fasanvej"],
+            "stationtype": "STATION_TYPE_COMBINED",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Frederiksberg"],
+            "stationtype": "STATION_TYPE_COMBINED",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Forum"],
+            "stationtype": "STATION_TYPE_COMBINED",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Nørreport"],
+            "stationtype": "STATION_TYPE_COMBINED",
+            "icons": [
+                ["S-tog"],
+                ["DSB"]
+            ]
+        },
+        {
+            "name": ["Kongens Nytorv"],
+            "stationtype": "STATION_TYPE_COMBINED",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Christianshavn"],
+            "stationtype": "STATION_TYPE_COMBINED",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Amagerbro"],
+            "stationtype": "STATION_TYPEM2",
+            "icons": [
+            ],
+            "dy": -16
+        },
+        {
+            "name": ["Lergravsparken"],
+            "stationtype": "STATION_TYPEM2",
+            "icons": [
+            ],
+            "dy": -16
+        },
+        {
+            "name": ["Øresund"],
+            "stationtype": "STATION_TYPEM2",
+            "icons": [
+            ],
+            "dy": -16
+        },
+        {
+            "name": ["Amager Strand"],
+            "stationtype": "STATION_TYPEM2",
+            "icons": [
+            ],
+            "dy": -16
+        },
+        {
+            "name": ["Femøren"],
+            "stationtype": "STATION_TYPEM2",
+            "icons": [
+            ],
+            "dy": -16
+        },
+        {
+            "name": ["Kastrup"],
+            "stationtype": "STATION_TYPEM2",
+            "icons": [
+            ],
+            "dy": -16
+        },
+        {
+            "name": ["Lufthavnen"],
+            "stationtype": "STATION_TYPEM2",
+            "icons": [
+                ["DSB"]
+            ],
+            "dy": -16
+        },
+        {
+            "name": ["Islands Brygge"],
+            "stationtype": "STATION_TYPEM1",
+            "icons": [
+            ],
+            "dy": 32
+        },
+        {
+            "name": ["DR Byen"],
+            "stationtype": "STATION_TYPEM1",
+            "icons": [
+            ],
+            "dy": 32
+        },
+        {
+            "name": ["Sundby"],
+            "stationtype": "STATION_TYPEM1",
+            "icons": [
+            ],
+            "dy": 32
+        },
+        {
+            "name": ["Bella Center"],
+            "stationtype": "STATION_TYPEM1",
+            "icons": [
+            ],
+            "dy": 32
+        },
+        {
+            "name": ["Ørestad"],
+            "stationtype": "STATION_TYPEM1",
+            "icons": [
+                ["DSB"]
+            ],
+            "dy": 32
+        },
+        {
+            "name": ["Vestamager"],
+            "stationtype": "STATION_TYPEM1",
+            "icons": [
+            ],
+            "dy": 32
+        }
+    ]
+};
+
 
 
