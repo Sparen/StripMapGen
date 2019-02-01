04a31163992e5e19b1a8ce5e20bca5d9949f64d1
Andrew Fan <andrewsuicune@gmail.com>
2019-02-01 18:22:27 -0500

    Began work on Guide 3; added guides 2 and 3 to sitemap

---
+++ user-guide/line_g3.js
@@ -0,0 +1,84 @@
+var obj_line3_v1 = {
+    "linename": "Station Set 1",
+    "iconID": ["ICON_GREEN"],
+    "strokes": [
+        {
+            "color": "#66AA66",
+            "strokewidth": "8px"
+        }
+    ],
+    "stationtypes": [
+        {
+            "stypeID": "STATION_TYPE1",
+            "stnnodes": [
+                {
+                    "stationstrokewidth": 4,
+                    "stationradius": 6,
+                    "scolor": "#66AA66"
+                }
+            ]
+        },
+        {
+            "stypeID": "STATION_TYPE2",
+            "stnnodes": [
+                {
+                    "stationstrokewidth": 4,
+                    "stationradius": 6,
+                    "scolor": "#66AA66"
+                }
+            ],
+            "stnfonttype": "'Times New Roman', 'serif'"
+        },
+        {
+            "stypeID": "STATION_TYPE3",
+            "stnnodes": [
+                {
+                    "stationstrokewidth": 4,
+                    "stationradius": 6,
+                    "scolor": "#66AA66"
+                }
+            ],
+            "stnfonttype": "'Times New Roman', 'serif'",
+            "stnfontsize": 20
+        },
+        {
+            "stypeID": "STATION_TYPE4",
+            "stnnodes": [
+                {
+                    "stationstrokewidth": 4,
+                    "stationradius": 6,
+                    "scolor": "#66AA66"
+                }
+            ],
+            "stnfonttype": "'Times New Roman', 'serif'",
+            "stnfontsize": 20,
+            "stnfontangle": 20
+        },
+        {
+            "stypeID": "STATION_TYPE5",
+            "stnnodes": [
+                {
+                    "stationstrokewidth": 4,
+                    "stationradius": 6,
+                    "scolor": "#66AA66"
+                }
+            ],
+            "stniconoffset": 32,
+            "stntextoffset": 32
+        }
+    ],
+    "stations": [
+        {
+            "name": ["Station Type 1 - Basic"],
+            "stationtype": "STATION_TYPE1",
+            "icons": [
+            ]
+        },
+        {
+            "name": ["Station Type 2 - stnfonttype"],
+            "stationtype": "STATION_TYPE2",
+            "icons": [
+            ]
+        }
+    ]
+};
